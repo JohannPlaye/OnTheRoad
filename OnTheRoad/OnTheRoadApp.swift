a14200d160b6cff674e87a1e7f1c8ee472f269a3
Johann <johannplaye@gmail.com>
2026-06-06 20:10:11 +0200

    Initial Commit

---
+++ OnTheRoad/OnTheRoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTheRoadApp.swift
+//  OnTheRoad
+//
+//  Created by Johann on 06/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OnTheRoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
